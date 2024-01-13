6dd722d6117a8add6f707a389bd5dd8a735fa6d1
Ryan Iskandar <teukurian090598@gmail.com>
2024-01-13 23:08:58 +0800

    "Modified to add percentage and colour changes"

--- src/components/stock-display/StockDisplay.jsx
+++ src/components/stock-display/StockDisplay.jsx
@@ -1,14 +1,30 @@
-import './stock-display.css'
+import './stock-display.css';
 
 export default function StockDisplay({ ticker, latestPrice, percentageChange, priceChange }) {
-    return (
-      <div className="stock-row-container">
-        <div className="stock-display">
-          <span className="ticker">{ticker}</span>
-          <span className="latest-price">{latestPrice}</span>
-          <span className="percentage-change">{percentageChange}</span>
-          <span className="price-change">{priceChange}</span>
-        </div>
+  const isPositiveChange = percentageChange >= 0;
+
+  const percentageChangeStyle = {
+    color: isPositiveChange ? '#1db954' : '#f45b5b',
+    fontSize: '1.2rem', // Adjust the font size as needed
+  };
+
+  const priceChangeStyle = {
+    color: isPositiveChange ? '#1db954' : '#f45b5b',
+    fontSize: '0.9rem', // Adjust the font size as needed
+  };
+
+  return (
+    <div className="stock-row-container">
+      <div className="stock-display">
+        <span className="ticker">{ticker}</span>
+        <span className="latest-price">{latestPrice}</span>
+        <span className="percentage-change" style={percentageChangeStyle}>
+          {percentageChange}%
+        </span>
+        <span className="price-change" style={priceChangeStyle}>
+          ({priceChange})
+        </span>
       </div>
-    );
-  }
+    </div>
+  );
+}
