f51fc58a5b100085bd49969468e9c17e72ae6c60
Danish Teuku <teukutiga@gmail.com>
2024-01-14 13:13:06 +0800

    refactor: rename files and restructure

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
-import './App.css'
-import './components/stock-display/stock-display.css'
-import StockDisplay from './components/stock-display/StockDisplay'
-import React from 'react';
-import { STOCK_DATA } from './components/stock-display/stock-data';
+import "./App.css";
+import Header from "./components/Header/Header";
+
+
 
 function App() {
   return (
-    <div>
-      <header className="stock-row-container">
-        {STOCK_DATA.map((stock, index) => (
-          <StockDisplay
-            key={index}
-            ticker={stock.ticker}
-            latestPrice={stock.latestPrice}
-            percentageChange={stock.percentageChange}
-            priceChange={stock.priceChange}
-          />
-        ))}
-      </header>
-      {/* The rest of your website content goes here */}
-    </div>
+    <Header/>
   );
 }
 
-export default App
+export default App;
