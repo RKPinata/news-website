066281000f698a2f09b7d5e569a6055ba209b63b
Danish Teuku <teukutiga@gmail.com>
2024-01-15 15:01:28 +0800

    fix: change comment on absolute imports

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,10 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 /** The alias is an implementation of absolute path imports in vite
  * read more: https://vitejs.dev/config/#resolve-alias
- * path.resolve(__dirname, "./src") is the absolute path of the src folder
- * so we can use @ to import files in src folder
- * The path module in the provided code is imported from the Node.js standard library
- * read more: https://nodejs.org/api/path.html
- *
- * ps. There are other ways to implement absolute path in vite with jsconfig.json or tsconfig.json
- * psx2. Try to use absolute path in your projects, it will make your life easier
+ * jsconfig.json is also a necessary file to tell vscode that you are using absolute path imports
+ * notice the alias "@" is also configured in jsconfig.json 
+ * 
+ * ps. Try to use absolute path in your projects, it will make your life easier
  * Thank you very much tehepero
  */
 
